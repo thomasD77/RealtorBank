c8d98f910b8759771f08e6de5f6365436014d84f
Ntriga.Agency <info@ntriga.agency>
2023-04-14 11:46:38 +0200

    technique andere fix

--- app/Http/Livewire/Technique/Elements/Brand.php
+++ app/Http/Livewire/Technique/Elements/Brand.php
@@ -16,5 +16,14 @@ class Brand extends MainDropdownComponent
     {
         //--> Custom
         $this->parameters = TechniqueArea::getBrands();
+
+        //--> Rendering 'Andere' text field
+        $this->dynamicArea = $dynamicArea;
+        $el = $this->element;
+        if(in_array($this->dynamicArea->$el, $this->parameters)){
+            $this->dynamic = null;
+        }else {
+            $this->dynamic = $this->dynamicArea->$el;
+        }
     }
 }
--- app/Http/Livewire/Technique/Elements/Model.php
+++ app/Http/Livewire/Technique/Elements/Model.php
@@ -16,5 +16,14 @@ class Model extends MainDropdownComponent
     {
         //--> Custom
         $this->parameters = TechniqueArea::getModels();
+
+        //--> Rendering 'Andere' text field
+        $this->dynamicArea = $dynamicArea;
+        $el = $this->element;
+        if(in_array($this->dynamicArea->$el, $this->parameters)){
+            $this->dynamic = null;
+        }else {
+            $this->dynamic = $this->dynamicArea->$el;
+        }
     }
 }
