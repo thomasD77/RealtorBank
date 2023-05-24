41906d45c0c291ffb4f72f32f1807c5ce6890a42
Ntriga.Agency <info@ntriga.agency>
2023-05-24 13:59:24 +0200

    test

--- resources/views/livewire/general/ground-floor-sorting.blade.php
+++ resources/views/livewire/general/ground-floor-sorting.blade.php
@@ -1,37 +1,40 @@
-<div class="col-md-6">
-    <strong class="w-100">{{ __('Gelijkvloers') }}</strong>
-    <div class="section-body listing-table">
-        <div class="table-responsive">
-            <table class="table table-striped" style="width: 100%">
-                <thead>
-                <tr>
-                    <th>{{ __('Titel') }}</th>
-                    <th>{{ __('Up') }}</th>
-                    <th>{{ __('Down') }}</th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($groundFloorParam as $item)
+<div>
+    <div class="col-md-6">
+        <strong class="w-100">{{ __('Gelijkvloers') }}</strong>
+        <div class="section-body listing-table">
+            <div class="table-responsive">
+                <table class="table table-striped" style="width: 100%">
+                    <thead>
                     <tr>
-                        <td>{{ $item->title }}</td>
-                        <td>
-                            @if($item->order != $minGround)
-                                <button class="btn btn-sm" wire:click="itemUp({{ $item }})">
-                                    <i class="fa fa-arrow-up"></i>
-                                </button>
-                            @endif
-                        </td>
-                        <td>
-                            @if($maxGround != $item->order)
-                                <button class="btn btn-sm" wire:click="itemDown({{ $item }})">
-                                    <i class="fa fa-arrow-down"></i>
-                                </button>
-                            @endif
-                        </td>
+                        <th>{{ __('Titel') }}</th>
+                        <th>{{ __('Up') }}</th>
+                        <th>{{ __('Down') }}</th>
                     </tr>
-                @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                    @foreach($groundFloorParam as $item)
+                        <tr>
+                            <td>{{ $item->title }}</td>
+                            <td>
+                                @if($item->order != $minGround)
+                                    <button class="btn btn-sm" wire:key="up-{{ $item->id }}" wire:click="itemUp({{ $item }})">
+                                        <i class="fa fa-arrow-up"></i>
+                                    </button>
+                                @endif
+                            </td>
+                            <td>
+                                @if($maxGround != $item->order)
+                                    <button class="btn btn-sm" wire:key="down-{{ $item->id }}" wire:click="itemDown({{ $item }})">
+                                        <i class="fa fa-arrow-down"></i>
+                                    </button>
+                                @endif
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
+
