d9b2a4194fd079cbdf6c87a26420523086425f5b
Ntriga.Agency <info@ntriga.agency>
2023-05-24 14:19:22 +0200

    tets

--- resources/views/livewire/general/ground-floor-sorting.blade.php
+++ resources/views/livewire/general/ground-floor-sorting.blade.php
@@ -1,40 +1,37 @@
-<div>
-    <div class="col-md-6">
-        <strong class="w-100">{{ __('Gelijkvloers') }}</strong>
-        <div class="section-body listing-table">
-            <div class="table-responsive">
-                <table class="table table-striped" style="width: 100%">
-                    <thead>
+<div class="col-md-6">
+    <strong class="w-100">{{ __('Gelijkvloers') }}</strong>
+    <div class="section-body listing-table">
+        <div class="table-responsive">
+            <table class="table table-striped" style="width: 100%">
+                <thead>
+                <tr>
+                    <th>{{ __('Titel') }}</th>
+                    <th>{{ __('Up') }}</th>
+                    <th>{{ __('Down') }}</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($groundFloorParam as $item)
                     <tr>
-                        <th>{{ __('Titel') }}</th>
-                        <th>{{ __('Up') }}</th>
-                        <th>{{ __('Down') }}</th>
+                        <td>{{ $item->title }}</td>
+                        <td>
+                            @if($item->order != $minGround)
+                                <button class="btn btn-sm" wire:key="up-{{ $item->id }}" wire:click="itemUp({{ $item }})">
+                                    <i class="fa fa-arrow-up"></i>
+                                </button>
+                            @endif
+                        </td>
+                        <td>
+                            @if($maxGround != $item->order)
+                                <button class="btn btn-sm" wire:key="down-{{ $item->id }}" wire:click="itemDown({{ $item }})">
+                                    <i class="fa fa-arrow-down"></i>
+                                </button>
+                            @endif
+                        </td>
                     </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($groundFloorParam as $item)
-                        <tr>
-                            <td>{{ $item->title }}</td>
-                            <td>
-                                @if($item->order != $minGround)
-                                    <button class="btn btn-sm" wire:key="up-{{ $item->id }}" wire:click="itemUp({{ $item }})">
-                                        <i class="fa fa-arrow-up"></i>
-                                    </button>
-                                @endif
-                            </td>
-                            <td>
-                                @if($maxGround != $item->order)
-                                    <button class="btn btn-sm" wire:key="down-{{ $item->id }}" wire:click="itemDown({{ $item }})">
-                                        <i class="fa fa-arrow-down"></i>
-                                    </button>
-                                @endif
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            </div>
+                @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
-
--- resources/views/livewire/general/upper-floor-sorting.blade.php
+++ resources/views/livewire/general/upper-floor-sorting.blade.php
@@ -1,39 +1,37 @@
-<div>
-    <div class="col-md-6">
-        <strong class="w-100">{{ __('Bovenverdieping') }}</strong>
-        <div class="section-body listing-table">
-            <div class="table-responsive">
-                <table class="table table-striped" style="width: 100%">
-                    <thead>
+<div class="col-md-6">
+    <strong class="w-100">{{ __('Bovenverdieping') }}</strong>
+    <div class="section-body listing-table">
+        <div class="table-responsive">
+            <table class="table table-striped" style="width: 100%">
+                <thead>
+                <tr>
+                    <th>{{ __('Titel') }}</th>
+                    <th>{{ __('Up') }}</th>
+                    <th>{{ __('Down') }}</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($upperFloorParam as $item)
                     <tr>
-                        <th>{{ __('Titel') }}</th>
-                        <th>{{ __('Up') }}</th>
-                        <th>{{ __('Down') }}</th>
+                        <td>{{ $item->title }}</td>
+                        <td>
+                            @if($minUpper != $item->order)
+                                <button class="btn btn-sm" wire:click="itemUp({{ $item }})">
+                                    <i class="fa fa-arrow-up"></i>
+                                </button>
+                            @endif
+                        </td>
+                        <td>
+                            @if($maxUpper != $item->order)
+                                <button class="btn btn-sm" wire:click="itemDown({{ $item }})">
+                                    <i class="fa fa-arrow-down"></i>
+                                </button>
+                            @endif
+                        </td>
                     </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($upperFloorParam as $item)
-                        <tr>
-                            <td>{{ $item->title }}</td>
-                            <td>
-                                @if($minUpper != $item->order)
-                                    <button class="btn btn-sm" wire:click="itemUp({{ $item }})">
-                                        <i class="fa fa-arrow-up"></i>
-                                    </button>
-                                @endif
-                            </td>
-                            <td>
-                                @if($maxUpper != $item->order)
-                                    <button class="btn btn-sm" wire:click="itemDown({{ $item }})">
-                                        <i class="fa fa-arrow-down"></i>
-                                    </button>
-                                @endif
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            </div>
+                @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
