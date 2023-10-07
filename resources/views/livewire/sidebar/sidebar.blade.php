38b7886512fae4c9ed6ac80df0fbaf9675fe9d90
Ntriga.Agency <info@ntriga.agency>
2023-10-07 11:25:45 +0200

    test sidebar

--- resources/views/livewire/sidebar/sidebar.blade.php
+++ resources/views/livewire/sidebar/sidebar.blade.php
@@ -95,7 +95,7 @@
 
                                                                 id="collapseBasic{{ $room->id }}"
                                                             >
-                                                                @foreach(\App\Models\BasicArea::where('room_id', $room->id)->orderBy('order', 'asc')->get() as $item)
+                                                                @foreach(\App\Models\BasicArea::where('room_id', $room->id)->orderBy('order', 'asc')->orderBy('sidebar_count', 'asc')->get() as $item)
                                                                     <li class="mx-3">
                                                                         <a class="@if($activeArea == $item->area->id) activeLink @endif" href="{{ route('area.detail', [$inspection, $room, $item->area]) }}">
                                                                             <i class="fa fa-chevron-right" aria-hidden="true"></i>{{ $item->sidebar_count }}@if($item->sidebar_count).@endif{{ $item->area->title }}
