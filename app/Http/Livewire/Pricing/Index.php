9db132583692d08dc60b7acd34d931dbcb50764b
Thomas Demeulenaere <thomas.demeulenaere@hotmail.com>
2024-09-10 12:50:34 +0200

    nice

--- app/Http/Livewire/Pricing/Index.php
+++ app/Http/Livewire/Pricing/Index.php
@@ -9,33 +9,118 @@ use Livewire\Component;
 
 class Index extends Component
 {
-    public $pricingCategories;
     public $categorySuccessMessages = [];
+
+    public $pricingCategories;
+    public $subCategories = []; // Voor subcategorieën die alleen horen bij de geselecteerde category
+    public $selectedPricing = null;
+    public $modalOpen = false;
+
+    // Pricing attributen
+    public $selectedCategoryId; // De geselecteerde category ID
+    public $selectedSubcategoryId; // Geselecteerde subcategory ID
+    public $selectedSubcategoryTitle; // Geselecteerde subcategory title (voor wijziging)
+    public $description;
+    public $cost_square_meter;
+    public $cost_hour;
+    public $cost_piece;
+
+    public $activeTab = 'select'; // Active tab, default to 'select'
 
     public function mount()
     {
-            $this->pricingCategories = CategoryPricing::all();
+        // Laad de pricing categories
+        $this->loadData();
     }
 
-    public function delete($pricingId)
+    public function loadData()
     {
-        $pricing = Pricing::find($pricingId);
-        if ($pricing) {
-            $categoryId = $pricing->category_pricing_id; // Stel de categorie-ID vast
-            $pricing->delete();
+        // Laad de pricing categories en subcategories
+        $this->pricingCategories = CategoryPricing::with('pricings.subCategoryPricing')->get();
+        $this->subCategories = SubCategoryPricing::all();
+    }
 
-            // Verwijder het record uit de lokale state voor die specifieke categorie
-            foreach ($this->pricingCategories as $category) {
-                if ($category->id === $categoryId) {
-                    $category->pricings = $category->pricings->filter(function ($item) use ($pricingId) {
-                        return $item->id !== $pricingId;
-                    });
+    public function loadSubCategories($categoryId)
+    {
+        // Laad de subcategories voor de geselecteerde category
+        $this->subCategories = SubCategoryPricing::where('category_pricing_id', $categoryId)->get();
+    }
 
-                    // Stel het succesbericht in voor de betreffende categorie
-                    $this->categorySuccessMessages[$categoryId] = 'Prijs succesvol verwijderd.';
-                }
-            }
+    public function edit($pricingId)
+    {
+        $this->selectedPricing = Pricing::with('subCategoryPricing')->find($pricingId);
+
+        // Haal de bijbehorende category ID op uit de geselecteerde pricing
+        $this->selectedCategoryId = $this->selectedPricing->category_pricing_id;
+
+        // Laad de subcategories voor de geselecteerde category
+        $this->loadSubCategories($this->selectedCategoryId);
+
+        // Subcategory info instellen
+        $this->selectedSubcategoryId = $this->selectedPricing->sub_category_pricing_id;
+        $this->selectedSubcategoryTitle = $this->selectedPricing->subCategoryPricing->title;
+        $this->description = $this->selectedPricing->description;
+        $this->cost_square_meter = $this->selectedPricing->cost_square_meter;
+        $this->cost_hour = $this->selectedPricing->cost_hour;
+        $this->cost_piece = $this->selectedPricing->cost_piece;
+
+        // Open de modal
+        $this->modalOpen = true;
+        $this->activeTab = 'select'; // Default naar 'select' tab
+    }
+
+    public function save()
+    {
+        $this->validate([
+            'description' => 'required|string|max:255',
+            'cost_square_meter' => 'nullable|numeric',
+            'cost_hour' => 'nullable|numeric',
+            'cost_piece' => 'nullable|numeric',
+        ]);
+
+        // Als een waarde leeg is, zet deze op null
+        $this->cost_square_meter = $this->cost_square_meter !== '' ? $this->cost_square_meter : null;
+        $this->cost_hour = $this->cost_hour !== '' ? $this->cost_hour : null;
+        $this->cost_piece = $this->cost_piece !== '' ? $this->cost_piece : null;
+
+        // Update de geselecteerde subcategory als de title is aangepast
+        if ($this->activeTab === 'edit') {
+            $this->validate(['selectedSubcategoryTitle' => 'required|string|max:255']);
+            $subcategory = SubCategoryPricing::find($this->selectedSubcategoryId);
+            $subcategory->title = $this->selectedSubcategoryTitle;
+            $subcategory->save();
         }
+
+        // Update de pricing informatie
+        $this->selectedPricing->sub_category_pricing_id = $this->selectedSubcategoryId;
+        $this->selectedPricing->description = $this->description;
+        $this->selectedPricing->cost_square_meter = $this->cost_square_meter;
+        $this->selectedPricing->cost_hour = $this->cost_hour;
+        $this->selectedPricing->cost_piece = $this->cost_piece;
+        $this->selectedPricing->save();
+
+        // Sluit de modal en reset de form
+        $this->modalOpen = false;
+        $this->resetForm();
+
+        // Haal de data opnieuw op om de tabel te updaten
+        $this->loadData();
+    }
+
+    public function setActiveTab($tab)
+    {
+        $this->activeTab = $tab;
+    }
+
+    public function resetForm()
+    {
+        $this->selectedSubcategoryId = null;
+        $this->selectedSubcategoryTitle = '';
+        $this->description = '';
+        $this->cost_square_meter = '';
+        $this->cost_hour = '';
+        $this->cost_piece = '';
+        $this->selectedPricing = null;
     }
 
     public function addPricing($categoryId)
@@ -56,6 +141,27 @@ class Index extends Component
                 // Voeg de nieuwe pricing toe aan de pricings van deze categorie
                 $category->pricings->push($pricing->load('subCategoryPricing'));
                 break;
+            }
+        }
+    }
+
+    public function delete($pricingId)
+    {
+        $pricing = Pricing::find($pricingId);
+        if ($pricing) {
+            $categoryId = $pricing->category_pricing_id; // Stel de categorie-ID vast
+            $pricing->delete();
+
+            // Verwijder het record uit de lokale state voor die specifieke categorie
+            foreach ($this->pricingCategories as $category) {
+                if ($category->id === $categoryId) {
+                    $category->pricings = $category->pricings->filter(function ($item) use ($pricingId) {
+                        return $item->id !== $pricingId;
+                    });
+
+                    // Stel het succesbericht in voor de betreffende categorie
+                   /* $this->categorySuccessMessages[$categoryId] = 'Prijs succesvol verwijderd.';*/
+                }
             }
         }
     }
--- resources/views/livewire/pricing/index.blade.php
+++ resources/views/livewire/pricing/index.blade.php
@@ -4,11 +4,11 @@
             <div class="row">
                 <div class="col-10">
                     <h3 class="">{{ $category->title }}</h3>
-                    @if (isset($categorySuccessMessages[$category->id]))
+                    {{--@if (isset($categorySuccessMessages[$category->id]))
                         <div class="btn btn-success flash_message mb-3">
                             {{ $categorySuccessMessages[$category->id] }}
                         </div>
-                    @endif
+                    @endif--}}
                 </div>
                 <div class="col-2 text-right">
                     <button wire:click="addPricing({{$category->id}})" class="btn-sm btn-common" style="border:none"><i class="fa fa-plus"></i>{{ $category->title }}</button>
@@ -49,12 +49,20 @@
                     <tbody>
                         @foreach($category->pricings as $pricing)
                             <tr>
-                                <td>{{ $pricing->subCategoryPricing->title }}</td>
-                                <td>{{ $pricing->description }}</td>
-                                <td>{{ $pricing->cost_square_meter }}</td>
-                                <td>{{ $pricing->cost_hour }}</td>
-                                <td>{{ $pricing->cost_piece }}</td>
-                                <td class="edit text-center"><a href=""><i class="fa fa-pencil-alt text-dark"></i></a></td>
+                                @if($pricing->subCategoryPricing->title == 'new')
+                                    <td><span class="badge badge-success">{{ $pricing->subCategoryPricing->title }}</span></td>
+                                @else
+                                    <td>{{ $pricing->subCategoryPricing->title }}</td>
+                                @endif
+                                <td @if($pricing->description === NULL) class="bg-secondary" @endif>{{ $pricing->description }}</td>
+                                <td @if($pricing->cost_square_meter === NULL) class="bg-secondary" @endif>{{ $pricing->cost_square_meter }}</td>
+                                <td @if($pricing->cost_hour === NULL) class="bg-secondary" @endif>{{ $pricing->cost_hour }}</td>
+                                <td @if($pricing->cost_piece === NULL) class="bg-secondary" @endif>{{ $pricing->cost_piece }}</td>
+                                <td class="edit text-center">
+                                    <a style="cursor: pointer" wire:click="edit({{ $pricing->id }})">
+                                        <i class="fa fa-pencil-alt text-dark"></i>
+                                    </a>
+                                </td>
                                 <td class="edit text-center"><a style="cursor: pointer" wire:click="delete({{$pricing->id}})"><i class="fa fa-trash text-danger"></i></a></td>
                             </tr>
                         @endforeach
@@ -63,6 +71,79 @@
             </div>
         </div>
     @endforeach
+
+    <!-- Modal -->
+    @if($modalOpen)
+        <div class="modal fade show" style="display: block;" tabindex="-1">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">{{ __('Bewerk Prijs') }}</h5>
+                        <button type="button" class="btn-close" wire:click="$set('modalOpen', false)"></button>
+                    </div>
+                    <div class="modal-body">
+                        <!-- Tab Navigatie -->
+                        <ul class="nav nav-tabs">
+                            <li class="nav-item">
+                                <a class="nav-link @if($activeTab === 'select') active bg-dark text-white @else bg-white text-dark  @endif" wire:click="setActiveTab('select')" href="#">{{ __('Selecteer (sub)categorie') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link @if($activeTab === 'edit') active bg-dark text-white @else bg-white text-dark  @endif" wire:click="setActiveTab('edit')" href="#">{{ __('Bewerk (sub)categorie') }}</a>
+                            </li>
+                        </ul>
+
+                        <!-- Tab Content -->
+                        <div class="tab-content mt-3">
+                            <!-- Selecteer Subcategory Tab -->
+                            @if($activeTab === 'select')
+                                <div class="mb-3">
+                                    <label for="selectedSubcategoryId" class="form-label">{{ __('(sub)Categorie') }}</label>
+                                    <select class="form-select" id="selectedSubcategoryId" wire:model="selectedSubcategoryId">
+                                        <option value="">{{ __('Selecteer een (sub)categorie') }}</option>
+                                        @foreach($subCategories as $subcategory)
+                                            <option value="{{ $subcategory->id }}">{{ $subcategory->title }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            @endif
+
+                            <!-- Bewerk Subcategory Tab -->
+                            @if($activeTab === 'edit')
+                                <div class="mb-3">
+                                    <label for="selectedSubcategoryTitle" class="form-label">{{ __('Wijzig (sub)Categorie titel') }}</label>
+                                    <input type="text" class="form-control" id="selectedSubcategoryTitle" wire:model="selectedSubcategoryTitle">
+                                </div>
+                            @endif
+
+                            <!-- Overige velden -->
+                            <div class="mb-3">
+                                <label for="description" class="form-label">Beschrijving</label>
+                                <input type="text" class="form-control" id="description" wire:model="description">
+                            </div>
+                            <div class="mb-3">
+                                <label for="cost_square_meter" class="form-label">€ / m2</label>
+                                <input type="number" class="form-control" id="cost_square_meter" wire:model="cost_square_meter">
+                            </div>
+                            <div class="mb-3">
+                                <label for="cost_hour" class="form-label">€ / uur</label>
+                                <input type="number" class="form-control" id="cost_hour" wire:model="cost_hour">
+                            </div>
+                            <div class="mb-3">
+                                <label for="cost_piece" class="form-label">€ / stuk</label>
+                                <input type="number" class="form-control" id="cost_piece" wire:model="cost_piece">
+                            </div>
+
+                            <!-- Opslaan en Annuleren knoppen -->
+                            <div class="d-flex justify-content-between">
+                                <button type="button" class="btn btn-dark" wire:click="save">{{ __('Opslaan') }}</button>
+                                <button type="button" class="btn btn-secondary" wire:click="$set('modalOpen', false)">{{ __('Annuleren') }}</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
 </div>
 
 
